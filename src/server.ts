6e1c3b58796a8a3532ba20ada24e0d78988c793f
yuval <yuvalc11499@gmail.com>
2022-02-15 19:13:09 +0200

    add middlewares for all routers

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,11 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { connectDb } from "./mongodb";
+import { usersRouter } from "./routers/users-router";
+import { productsRouter } from "./routers/products-router";
+import { categoriesRouter } from "./routers/categories-router";
+import { cartsRouter } from "./routers/carts-router";
+import { cartItemsRouter } from "./routers/cart-items-router";
 
 config();
 const { PORT } = process.env;
@@ -16,6 +21,12 @@ const CORS_CONFIG = {
 app.use(cors(CORS_CONFIG));
 app.use(cookieParser());
 app.use(express.json());
+
+app.use("/users", usersRouter);
+app.use("/products", productsRouter);
+app.use("/categories", categoriesRouter);
+app.use("/shopping-carts", cartsRouter);
+app.use("/cart-items", cartItemsRouter);
 
 startServer();
 
